58466831a1a6ec86ee9d8fa4a44b5425eb157db3
sreemantadas <dassreemanta15august@gmail.com>
2025-02-19 11:22:30 +0530

    Update g.tf

--- g.tf
+++ g.tf
@@ -1,21 +1,21 @@
-resource "azurerm_resource_group" "example" {
-    name = "new-rg2"
-    location = "eastus2"
+#resource "azurerm_resource_group" "example" {
+  #  name = "new-rg2"
+ #   location = "eastus2"
   
-}
-resource "azurerm_resource_group" "example1" {
-    name = "new-rg3"
-    location = "eastus2"
+#}
+#resource "azurerm_resource_group" "example1" {
+  #  name = "new-rg3"
+ #   location = "eastus2"
   
-}
-resource "azurerm_resource_group" "example3" {
-    name = "new-rg4"
-    location = "eastus"
+#}
+#resource "azurerm_resource_group" "example3" {
+  #  name = "new-rg4"
+ #   location = "eastus"
   
-}
+#}
 
-resource "azurerm_resource_group" "example4" {
-    name = "new-rg5"
-    location = "eastus"
+#resource "azurerm_resource_group" "example4" {
+  #  name = "new-rg5"
+ #   location = "eastus"
   
-}
+#}
