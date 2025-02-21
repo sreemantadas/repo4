7e6af7cc57b0cd14725b3f9b1d3142e9947b5a7b
sreemantadas <dassreemanta15august@gmail.com>
2025-02-21 17:07:05 +0530

    Delete the resources

--- g.tf
+++ g.tf
@@ -11,11 +11,11 @@
 #resource "azurerm_resource_group" "example3" {
   #  name = "new-rg4"
  #   location = "eastus"
-  
+#  
 #}
 
 #resource "azurerm_resource_group" "example4" {
   #  name = "new-rg5"
  #   location = "eastus"
-  
+#  
 #}
