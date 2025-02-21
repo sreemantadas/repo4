98b4e9e8594d8b17e93e6d30f3d6ce149124d99c
sreemantadas <dassreemanta15august@gmail.com>
2025-02-21 14:45:04 +0530

    Updated resource group

--- g.tf
+++ g.tf
@@ -8,14 +8,14 @@
  #   location = "eastus2"
   
 #}
-#resource "azurerm_resource_group" "example3" {
-  #  name = "new-rg4"
- #   location = "eastus"
+resource "azurerm_resource_group" "example3" {
+    name = "new-rg4"
+    location = "eastus"
   
-#}
+}
 
-#resource "azurerm_resource_group" "example4" {
-  #  name = "new-rg5"
- #   location = "eastus"
+resource "azurerm_resource_group" "example4" {
+    name = "new-rg5"
+    location = "eastus"
   
-#}
+}
